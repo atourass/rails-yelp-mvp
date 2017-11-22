56983af045facf7aee4a1953cebfea40bcfc7723
Antoine Tourasse <antoine.tourasse@gmail.com>
2017-11-22 17:23:36 +0100

    Tout sauf improve

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "restaurant#index"
   resources :restaurants, only: [:show, :index, :new, :create] do
-    resources :reviews, only: [:index, :new, :create]
+    resources :reviews, only: [:new, :create]
   end
 
 end
