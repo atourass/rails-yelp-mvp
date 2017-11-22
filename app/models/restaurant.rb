7bbfe4e9a9c57da7e32fce58da19fef2d262eb71
Antoine Tourasse <antoine.tourasse@gmail.com>
2017-11-22 18:59:08 +0100

    Footer en +

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, :address, presence: true
   validates :category, inclusion: { in: CATEGORIES,
   message: "%{value} is not a valid category" }
-  has_many :reviews, dependent: :destroy
+
 
 end
